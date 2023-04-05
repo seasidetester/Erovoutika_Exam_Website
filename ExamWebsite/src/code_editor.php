5f4c7170025971d5cda2e356d4bbda2075d0652c
lalelilawrence <johnlawrenceencinada@gmail.com>
2023-04-05 16:06:46 +0800

    Update code_editor.php

--- ExamWebsite/src/code_editor.php
+++ ExamWebsite/src/code_editor.php
@@ -19,8 +19,8 @@
 
 <style>
     :root {
-    --dark-blue: #14007a;
-    --blue: #1a009c;
+    --dark-blue: #003a8c;
+    --blue: #3b6ca4;
     --light-blue: rgb(74, 225, 255);
     --pink: rgb(255, 40, 113);
 }
@@ -33,8 +33,8 @@
 
 body {
     height: 100vh;
-    font-family: sans-serif;
-    background-image: linear-gradient(45deg,var(--light-blue), var(--blue));
+    font-family: Consolas;
+    background-image: linear-gradient(90deg, rgba(63,94,251,1) 0%, rgba(70,196,252,1) 100%);
 
     display: flex;
     justify-content: center;
@@ -42,48 +42,61 @@ body {
 }
 
 .code-editor {
-    width: 90vw;
-    height: 90vh;
-
+    width: 100vw;
+    height: 100vh;
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     background-color: #fff;
-    border-radius: 1rem;
-    overflow: hidden;
-    border: 1px solid var(--dark-blue);
+   overflow: hidden;
+resize: horizontal;
+	
 }
 
 .code {
     display: grid;
-    grid-template-rows: repeat(3, 1fr);
-    overflow-y: auto;
 
+    grid-template-rows: repeat(10, 2fr);
+    overflow-y: auto;
+	
     background-color: var(--dark-blue);
     padding: 1rem;
-    border-radius: 0 1rem 1rem 0;
+    
+	
+	
 }
 
 h1 {
-    font: 600 1.2rem sans-serif;
-    margin: 1rem 0;
+    font: 500 1.2rem sans-serif;
+	font-weight: bold;
+    margin-top: 50px;
     color: #fff;
+	
+
+	
 }
 
 h1 > img {
     width: 1.3rem;
     margin-right: 1rem;
+	
     vertical-align: middle;
+	
 }
 
 .code textarea {
     width: 100%;
-    height: calc(100% - 4rem);
+	margin-top:20px;
+    height: calc(11rem - 3rem);
     background-color: var(--blue);
-    color: var(--light-blue);
+    color: white;
     border: none;
     padding: 1rem;
-    font-size: 1.1rem;
     resize: vertical;
+	font-size:16px;
+	border:solid 1px #7595BF;
+	
+
+	
 }
 
 .code textarea::-webkit-scrollbar {
@@ -101,35 +114,97 @@ h1 > img {
 }
 
 a {
-    background-color: lime;
+
     color: #fff;
-    padding: 0 1.2rem;
-    border-radius: .5rem;
-    text-decoration: none;
-    font-size: 2rem;
-    float: right;
-    cursor: pointer;
+  
 }
+
+.header{
+	background-color: #003a8c;
+	width: 100%;
+	height: 8vh;
+	margin-top:-600px;
+	position:fixed;
+	border-radius: 0 0 1rem 1rem;
+}
+
+#img{
+	margin-left:20px;
+	
+}
+
+
+html-code{
+	margin-top:-50px;
+}
+
+
+.footer{
+	background-color: #003a8c;
+	width: 100%;
+	height: 8vh;
+	margin-top:15px;
+	position:fixed;
+	border-radius: 1rem 1rem 0 0 ;
+}
+
+.html-code{
+	margin-top: -50px;
+}
+
+.css-code{
+	margin-top: -15px;
+}
+
+.js-code{
+	margin-top: -5px;
+}
+
+#img1{
+	width:120px;
+	margin-top:-10px;
+	
+}
+
+
+
+#bck{
+	font-family: sans-serif;
+	color: white;
+	font-weight: bold;
+	font-size: 15px;
+	margin-top:-50px;
+	margin-left:585px;
+	padding: 0 10px 0 0;
+}
+
 </style>
 
 <body>
+
+
     <div class="code-editor">
+	
         <div class="code">
+		<a href="learn.php"><img id="img1" src="images/Blue BG Logo.png" alt=""></a>
+		
+		<h1 id="bck"><a href="learn.php">◄ Back to Tutorial</a></h1>
             <div class="html-code">
-                <h1><img src="images/html.png" alt="">HTML</h1>
+                <h1><img src="images/html_logo.png" alt="">HTML</h1>
                 <textarea></textarea>
             </div>
             <div class="css-code">
-                <h1><img src="images/CSS.png" alt="">CSS</h1>
+                <h1><img src="images/css_logo.png" alt="">CSS</h1>
                 <textarea></textarea>
             </div>
             <div class="js-code">
-                <h1><img src="images/js.png" alt="">JS</h1>
+                <h1><img src="images/js_logo.png" alt="">JAVASCRIPT</h1>
                 <textarea spellcheck="false"></textarea>
             </div>
         </div>
         <iframe id="result"></iframe>
     </div>
+	
 
     <script src="script.js"></script>
 </body>
